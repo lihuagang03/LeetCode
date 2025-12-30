8253eabea981017769edb0aa4867d0bd70c3f177
lihuagang03 <bert825_work@163.com>
2025-12-30 17:06:59 +0800

    7.01 连续的子数组和 523. 连续的子数组和

--- src/main/java/exam/problems/continuous/subarray/sum/Main.java
+++ src/main/java/exam/problems/continuous/subarray/sum/Main.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 import java.util.Set;
 
 /**
- * 523. 连续的子数组和
  * <a href="https://pintia.cn/problem-sets/2004497238921478144/exam/problems/type/7?problemSetProblemId=2004497238955032576">
  *     7.01 连续的子数组和</a>
+ *
+ * <a href="https://leetcode.cn/problems/continuous-subarray-sum/description/">
+ *     523. 连续的子数组和</a>
  */
 public class Main {
 
