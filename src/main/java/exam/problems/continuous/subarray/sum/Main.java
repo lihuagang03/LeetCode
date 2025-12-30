c262d0d2fed7539fc363ae4273af658899e28a38
lihuagang03 <bert825_work@163.com>
2025-12-30 14:31:45 +0800

    连续的子数组和

---
+++ src/main/java/exam/problems/continuous/subarray/sum/Main.java
@@ -0,0 +1,43 @@
+package exam.problems.continuous.subarray.sum;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            String numLine = in.nextLine();
+            int k = in.nextInt();
+
+            String[] numArray = numLine.split(",");
+
+            int length = numArray.length;
+            int[] nums = new int[length];
+            for (int i = 0; i < length; i++) {
+                nums[i] = Integer.parseInt(numArray[i]);
+            }
+
+            // 前缀和
+            int[] sums = new int[length + 1];
+            for (int i = 1; i <= length; i++) {
+                sums[i] = sums[i - 1] + nums[i - 1];
+            }
+
+            // 同余定理
+            // 哈希表
+            Set<Integer> set = new HashSet<>();
+            for (int i = 2; i <= length; i++) {
+                set.add(sums[i - 1] % k);
+                if (set.contains(sums[i] % k)) {
+                    System.out.println(1);
+                    return;
+                }
+            }
+            System.out.println(0);
+        }
+    }
+
+}
