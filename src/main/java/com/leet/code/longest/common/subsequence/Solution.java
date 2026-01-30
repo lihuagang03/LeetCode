1e19705d779fff8c34c59f65782c3d007a668051
lihuagang03 <bert825_work@163.com>
2026-01-30 20:10:25 +0800

    1143. 最长公共子序列

---
+++ src/main/java/com/leet/code/longest/common/subsequence/Solution.java
@@ -0,0 +1,34 @@
+package com.leet.code.longest.common.subsequence;
+
+/**
+ * <a href="https://leetcode.cn/problems/longest-common-subsequence/description/">
+ *     1143. 最长公共子序列</a>
+ *
+ * @author lihuagang
+ */
+public class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        // 定义：s1[0..i-1] 和 s2[0..j-1] 的 lcs 长度为 dp[i][j]
+        // 目标：s1[0..m-1] 和 s2[0..n-1] 的 lcs 长度，即 dp[m][n]
+        // base case: dp[0][..] = dp[..][0] = 0
+        int[][] dp = new int[m+1][n+1];
+
+        // 状态转移方程
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                // 现在 i 和 j 从 1 开始，所以要减一
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    // s1[i-1] 和 s2[j-1] 必然在 lcs 中
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    // s1[i-1] 和 s2[j-1] 至少有一个不在 lcs 中
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
