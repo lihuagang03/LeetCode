865997c68b56cd02ffbe432d48d9269a62fca445
lihuagang03 <bert825_work@163.com>
2026-02-01 17:21:08 +0800

    3. 无重复字符的最长子串

---
+++ src/main/java/com/leet/code/longest/substring/without/repeating/characters/Solution.java
@@ -0,0 +1,38 @@
+package com.leet.code.longest.substring.without.repeating.characters;
+
+/**
+ * <a href="https://leetcode.cn/problems/longest-substring-without-repeating-characters/description/">
+ *     3. 无重复字符的最长子串</a>
+ *
+ * @author lihuagang
+ */
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        // 滑动窗口计数器
+        int[] window = new int[128];
+        // 最长子串 的长度
+        int maxLen = 0;
+
+        int left = 0;
+        int right = 0;
+        int n = s.length();
+        while (right < n) {
+            char c = s.charAt(right);
+            right++;
+            // 进行窗口内数据的一系列更新
+            window[c]++;
+
+            // 判断左侧窗口是否要收缩
+            while (window[c] > 1) {
+                char d = s.charAt(left);
+                left++;
+                window[d]--;
+            }
+            maxLen = Math.max(maxLen, right - left);
+        }
+        return maxLen;
+    }
+}
