2adc50607fcd0c8af9e974a4a568ddeb5d6f91d9
lihuagang03 <bert825_work@163.com>
2026-02-04 15:30:18 +0800

    Improve 7.05 超级回文数 906. 超级回文数

--- src/main/java/com/leet/code/superstar/palindromes/Solution.java
+++ src/main/java/com/leet/code/superstar/palindromes/Solution.java
@@ -10,10 +10,13 @@ public class Solution {
     public int superPalindromesInRange(String left, String right) {
         long L = Long.parseLong(left);
         long R = Long.parseLong(right);
+        // 1 <= left.length, right.length <= 18
+        // 同时 R=k∥k′（两串数字连接），其中 k′ 是 k 的反序（也有可能截掉了中间数字），所以 k<10^5=MAGIC，我们的神奇常数。
         int MAGIC = 100_000;
         int ans = 0;
 
         // count odd length
+        // left 和 right 表示的整数在区间 [1, 10^18 - 1] 内。
         for (int k = 1; k < MAGIC; k++) {
             // 假如 k=1234 那么 R=1234321
             StringBuilder sb = new StringBuilder(Integer.toString(k));
@@ -32,12 +35,14 @@ public class Solution {
         }
 
         // count even length
+        // left 和 right 表示的整数在区间 [1, 10^18 - 1] 内。
         for (int k = 1; k < MAGIC; k++) {
             // 假如 k=1234 那么 R=12344321
             StringBuilder sb = new StringBuilder(Integer.toString(k));
             for (int i = sb.length() - 1; i >= 0; i--) {
                 sb.append(sb.charAt(i));
             }
+            // 假设 P=R^2 是超级回文数
             long v = Long.parseLong(sb.toString());
             v *= v;
             if (v > R) {
--- src/main/java/exam/problems/superstar/palindromes/Main.java
+++ src/main/java/exam/problems/superstar/palindromes/Main.java
@@ -23,16 +23,23 @@ public class Main {
             return;
         }
         String[] parts = s.split(",");
+
         long L = Long.parseLong(parts[0]);
         long R = Long.parseLong(parts[1]);
+        // 1 <= left.length, right.length <= 18
+        // 同时 R=k∥k′（两串数字连接），其中 k′ 是 k 的反序（也有可能截掉了中间数字），所以 k<10^5=MAGIC，我们的神奇常数。
         int MAGIC = 100_000;
         List<Long> ans = new ArrayList<>();
 
+        // count odd length
+        // left 和 right 表示的整数在区间 [1, 10^18 - 1] 内。
         for (int k = 1; k < MAGIC; k++) {
+            // 假如 k=1234 那么 R=1234321
             StringBuilder sb = new StringBuilder(Integer.toString(k));
             for (int i = sb.length() - 2; i >= 0; i--) {
                 sb.append(sb.charAt(i));
             }
+            // 假设 P=R^2 是超级回文数
             long v = Long.parseLong(sb.toString());
             v *= v;
             if (v > R) {
@@ -42,12 +49,15 @@ public class Main {
                 ans.add(v);
             }
         }
-
+        // count even length
+        // left 和 right 表示的整数在区间 [1, 10^18 - 1] 内。
         for (int k = 1; k < MAGIC; k++) {
+            // 假如 k=1234 那么 R=12344321
             StringBuilder sb = new StringBuilder(Integer.toString(k));
             for (int i = sb.length() - 1; i >= 0; i--) {
                 sb.append(sb.charAt(i));
             }
+            // 假设 P=R^2 是超级回文数
             long v = Long.parseLong(sb.toString());
             v *= v;
             if (v > R) {
@@ -57,7 +67,6 @@ public class Main {
                 ans.add(v);
             }
         }
-
         ans.sort(Long::compareTo);
         System.out.println(ans);
     }
