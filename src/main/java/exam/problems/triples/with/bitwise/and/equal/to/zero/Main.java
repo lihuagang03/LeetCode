a85a9828d0eda0b63506fddbdda17d09929733c6
lihuagang03 <bert825_work@163.com>
2026-02-04 10:30:22 +0800

    Improve 7.09 按位与为零的三元组 982. 按位与为零的三元组

--- src/main/java/exam/problems/triples/with/bitwise/and/equal/to/zero/Main.java
+++ src/main/java/exam/problems/triples/with/bitwise/and/equal/to/zero/Main.java
@@ -27,16 +27,22 @@ public class Main {
             nums[i] = Integer.parseInt(parts[i]);
         }
 
-        int D = 1 << 16;
-        int[] cnt = new int[D];
+        // 0 <= nums[i] < 2^16
+        int N = 1 << 16;
+        // 用哈希表实现的 cnt 要枚举所有 key；用数组实现的 cnt 要枚举区间 [0,2^16) 内的所有数（根据题目的数据范围得出）。
+        int[] cnt = new int[N];
+        // 可以先写一个 O(n^2) 的枚举，预处理所有 nums[i] AND nums[j] 的出现次数，存到一个哈希表（或者数组）cnt 中。
         for (int x : nums) {
             for (int y : nums) {
+                // nums[i] & nums[j] & nums[k] == 0
                 cnt[x & y]++;
             }
         }
+        // 然后枚举 nums[k] 和 x，如果 nums[k] AND x 等于 0，那就把 cnt[x] 加到答案中。
         int ans = 0;
         for (int x : nums) {
-            for (int y = 0; y < D; y++) {
+            for (int y = 0; y < N; y++) {
+                // nums[i] & nums[j] & nums[k] == 0
                 if ((x & y) == 0) {
                     ans += cnt[y];
                 }
